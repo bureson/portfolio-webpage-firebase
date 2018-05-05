e6b538a750d28ec8ecdc02441590623515d079f8
Ondrej Bures <o.bures@gmail.com>
2018-05-05 15:10:52 +0200

    Improved pager

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -19,17 +19,27 @@ class Pager extends Component {
   }
 
   render = () => {
-    const pageCount = Math.ceil(this.state.itemsCount / this.state.perPage);
+    const totalPageCount = Math.ceil(this.state.itemsCount / this.state.perPage);
+    const pageCount = Math.min(totalPageCount, 10); // Note: max 10 pages
+    const pageDiff = totalPageCount - this.state.page;
+    const closeToEnd = pageDiff < 6;
+    const deduction = closeToEnd ? 9 - pageDiff : 3;
+    const firstPage = Math.max(this.state.page - deduction, 1);
+    const lastIndex = Math.max(totalPageCount - 1, 0);
     return (
       <nav className='pager'>
         <ul>
+          <li><button onClick={e => this.props.onPageChange(e, 0)}>&lt;&lt;</button></li>
           {[...Array(pageCount).keys()].map(i => {
+            const page = i + firstPage;
+            const index = page - 1;
             return (
-              <li key={i} className={i === this.state.page ? 'active' : undefined}>
-                <button onClick={e => this.props.onPageChange(e, i)}>{i + 1}</button>
+              <li key={i} className={index === this.state.page ? 'active' : undefined}>
+                <button onClick={e => this.props.onPageChange(e, index)}>{page}</button>
               </li>
             )
           })}
+          <li><button onClick={e => this.props.onPageChange(e, lastIndex)}>&gt;&gt;</button></li>
         </ul>
       </nav>
     )
