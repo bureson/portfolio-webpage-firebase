fc8d924acb7e6629f561865c9c4e3ff8177846c9
Ondrej Bures <o.bures@gmail.com>
2018-09-03 08:38:15 +0200

    Fix build

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -116,7 +116,7 @@ class Course extends Component {
         <p>
           After running the <em>Word of the day</em> for more than a year, on the 25th of July 2018 we decided that it would be nice to round up the challenge at the number 300.
           Since then we have managed to come up with a couple of good words that are worth noting down here and we might occasionally do so in the future, but
-          we consider this initiative to be finished. A giant thanks belongs to my colleague <a href='http://ianvictor.dk/' target='_blank'>Ian Abildskou</a>,
+          we consider this initiative to be finished. A giant thanks belongs to my colleague <a href='http://ianvictor.dk/' target='_blank' rel='noopener noreferrer'>Ian Abildskou</a>,
           because this brought us, but not only us a lot of fun.
         </p>
         <div className='page-header'>
