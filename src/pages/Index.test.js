61ff4d95ab82cfc29f57472852ebe3ecaa6aae42
Ondrej Bures <o.bures@gmail.com>
2019-03-17 17:09:25 +0100

    Fix test

--- src/pages/Index.test.js
+++ src/pages/Index.test.js
@@ -15,7 +15,7 @@ describe('pages/Index', () => {
     }));
     const wrapper = shallow(<Index match={{path: '/'}} />);
     const routeList = wrapper.find('Switch').find('Route');
-    expect(routeList).toHaveLength(13);
+    expect(routeList).toHaveLength(14);
     routeList.forEach(route => {
       route.props().render && route.props().render();
     });
