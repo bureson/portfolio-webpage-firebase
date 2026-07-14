9fa6cf41aeabd58e87119b261cfade36a1de61e3
Ondrej Bures <o.bures@gmail.com>
2026-07-14 09:21:54 +0200

    Goat Counter

--- src/components/RouteChangeListener.js
+++ src/components/RouteChangeListener.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const RouteChangeListener = props => {
+  // GoatCounter counts the initial page load itself; only SPA navigations
+  // after that need to be reported manually.
+  const isInitialLoad = React.useRef(true);
   React.useEffect(() => {
     window.scrollTo(0, 0);
-  }, [props.location.pathname]);
+    if (isInitialLoad.current) {
+      isInitialLoad.current = false;
+      return;
+    }
+    if (window.goatcounter) {
+      window.goatcounter.count({
+        path: props.location.pathname + props.location.search,
+      });
+    }
+  }, [props.location.pathname, props.location.search]);
   return null;
 };
 
